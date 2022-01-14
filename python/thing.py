31f6efa93f7dab368e29ed97fb9780551394429c
rsaeta <rsaeta@gmail.com>
2022-01-14 11:22:51 +0100

    We want to say hi to the team

--- python/thing.py
+++ python/thing.py
@@ -8,6 +8,8 @@ $ python git:(python) ✗ time python3 thing.py
   python3 thing.py  90.08s user 0.86s system 94% cpu 1:36.01 total
 """
 
+print('hello team')
+
 def play_itr() -> int:
     deck = list(range(52))
     random.shuffle(deck)
